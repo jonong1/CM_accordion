3acb8bfb5cbe9573f7829dd3f95d63e5f00f4996
jonong1 <jzong@ualberta.ca>
2022-04-19 22:04:32 -0600

    update

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-var button = document.getElementById("btn");
-button.addEventListener('click', function() {
-  if (button.getAttribute("data-text-swap") == button.innerHTML) {
-    button.innerHTML = button.getAttribute("data-text-original");
-  } else {
-    button.setAttribute("data-text-original", button.innerHTML);
-    button.innerHTML = button.getAttribute("data-text-swap");
-  }
-}, false);
-
 const accordion = document.getElementsByClassName('container');
 for (i=0; i<accordion.length; i++) {
   accordion[i].addEventListener('click', function () {
     this.classList.toggle('active')
   })
 }
+
+var button = document.getElementById("btn");
+button.addEventListener('click', function() {
+  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+    for (i=0; i<accordion.length; i++) {
+      if (accordion[i].classList.toggle('active')) {
+        accordion[i].classList.toggle('active')
+      }
+    }
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+    for (i=0; i<accordion.length; i++) {
+      if (!accordion[i].classList.toggle('active')) {
+        accordion[i].classList.toggle('active')
+      }
+    }
+  }
+}, false);
